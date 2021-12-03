762056d90d78b4972a35d181b69a92b4f554351b
Julian Korgol <julian.korgol@gmail.com>
2021-12-03 21:17:41 +0100

    Plugin configuration

---
+++ WordPress-CloudFlare-cache-purge/WordPress-CloudFlare-cache-purge.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: CloudFlare cache purge
+ * Plugin URI: https://github.com/JulianKorgol/WordPress-CloudFlare-cache-purge
+ * Description: This plugin allow you to purge CloudFlare Cache by API.
+ * Version: 1.0
+ * Author: Julian Korgol, Mikołaj Mroczkowski
+ * Author URI: https://github.com/JulianKorgol/WordPress-CloudFlare-cache-purge
+ * License:     Apache License 2.0
+ * License URI: https://github.com/JulianKorgol/WordPress-CloudFlare-cache-purge/blob/main/LICENSE
+ */
+
+
+?>
